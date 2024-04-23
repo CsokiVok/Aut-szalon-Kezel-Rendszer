d3c7f332196866e73d35b6014c309ca847b0754a
CsokiVok <144450992+CsokiVok@users.noreply.github.com>
2024-04-23 12:54:56 +0200

    Add files via upload

---
+++ Autószalon Kezelő Rendszer/Autószalon Kezelő Rendszer/Autosalon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autószalon_Kezelő_Rendszer
+{
+	internal class Autosalon
+	{
+		public List<Jarmu> Jarmuvek { get; set; }
+		public Autosalon()
+		{
+			Jarmuvek = new List<Jarmu>();
+		}
+		public void UjJarmuHozzaadasa(Jarmu jarmu)
+		{
+			Jarmuvek.Add(jarmu);
+            Console.WriteLine("Sikeresen hozzáadva a listához.");
+
+        }
+		public void JarmuEladasa(string gyarto, string modell)
+		{
+			bool kimenet = false;
+			foreach (var item in Jarmuvek)
+			{
+				if (item.Gyarto == gyarto && item.Modell == modell)
+				{
+					Jarmuvek.Remove(item);
+					kimenet = true;
+				}
+			}
+			if (kimenet == false)
+			{
+                Console.WriteLine("Az eladás sikertelen");
+            }
+			else
+			{
+                Console.WriteLine("Az eladás sikeres");
+            }
+		}
+		public void JarmuKeresese(string gyarto, string modell)
+		{
+			foreach(var item in Jarmuvek)
+			{
+				if(item.Gyarto == gyarto && item.Modell == modell)
+				{
+                    Console.WriteLine(item);
+                }
+			}
+		}
+
+		public void JarmuvekListazasa()
+		{
+			foreach (var item in Jarmuvek)
+			{
+                Console.WriteLine($" Gyártó:{item.Gyarto}, Modell:{item.Modell}, Évjárat:{item.Evjarat}, Alapár: {item.Alapar}, Kilométeróra állás: {item.KilometeroraAllas}");
+            }
+		}
+	}
+}
